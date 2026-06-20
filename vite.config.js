167291a66161bd1ca49a040661151ef62639d2a4
Justice <londonjustice176@gmail.com>
2026-06-19 21:08:24 -0700

    fixed the blank page problem

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/apply/',
-  server: {
-    port: 3000,
-    open: true
-  }
+  plugins: [react()]
 })
